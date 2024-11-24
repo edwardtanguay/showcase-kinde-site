83d17d9e7d56f5fb67b67620fc56ea2c442d642c
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-24 10:52:11 +0100

    links in

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ function App() {
     <>
       <h1>Showcase Kinde</h1>
       <p>This is the kinde site.</p>
+      <p>repo: <a href="https://github.com/edwardtanguay/showcase-kinde-site" target="_blank">https://github.com/edwardtanguay/showcase-kinde-site</a></p>
+      <p>site: <a href="https://showcase-kinde-site.vercel.app" target="_blank">https://showcase-kinde-site.vercel.app</a></p>
     </>
   )
 }
