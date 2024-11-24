b3226c7dce501dd484ce8fe1aba328572aad23cc
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-24 10:53:50 +0100

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 function App() {
-
-  return (
-    <>
-      <h1>Showcase Kinde</h1>
-      <p>This is the kinde site.</p>
-      <p>repo: <a href="https://github.com/edwardtanguay/showcase-kinde-site" target="_blank">https://github.com/edwardtanguay/showcase-kinde-site</a></p>
-      <p>site: <a href="https://showcase-kinde-site.vercel.app" target="_blank">https://showcase-kinde-site.vercel.app</a></p>
-    </>
-  )
+	return (
+		<>
+			<h1>Showcase Kinde</h1>
+			<p>This is the kinde site.</p>
+			<p>
+				repo:{" "}
+				<a
+					href="https://github.com/edwardtanguay/showcase-kinde-site"
+					target="_blank"
+				>
+					https://github.com/edwardtanguay/showcase-kinde-site
+				</a>
+			</p>
+			<p>
+				site:{" "}
+				<a
+					href="https://showcase-kinde-site.vercel.app"
+					target="_blank"
+				>
+					https://showcase-kinde-site.vercel.app
+				</a>
+			</p>
+		</>
+	);
 }
 
-export default App
+export default App;
